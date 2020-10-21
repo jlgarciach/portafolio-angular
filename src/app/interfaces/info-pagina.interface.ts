bce2cfd4c5ea2b622d7115f748e130e9db9a1001
jlgarciach <56977476+jlgarciach@users.noreply.github.com>
2020-10-21 14:09:01 -0600

    Integración de Servicios con Firebase

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+    titulo?:         string;
+    email?:          string;
+    nombre_autor?:   string;
+    facebook?:       string;
+    twitter?:        string;
+    instagram?:      string;
+    tumblr?:         string;
+    equipo_trabajo?: any[];
+}
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient) {
+    //console.log("Servicio de infoPagina listo!!!");
+
+    // Leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+        //console.log(resp['email']);
+        //console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://platzi-1553705543960.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log( this.equipo );
+    })
+  }
+
+
+}
